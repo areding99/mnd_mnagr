d96a3a47b59792d30fdac4060f17db69f85f27ea
Andy Reding <areding99@gmail.com>
2024-01-24 18:13:38 +0000

    pkm's 'move file'

--- example/pkm/work/sql_cookbook.md
+++ example/pkm/work/sql_cookbook.md
@@ -1,8 +1,8 @@
 ---
 title: sql_cookbook
-path: example/pkm/work/sql_cookbook.md
+path: pkm/work/sql_cookbook.md
 created: 2024-01-23 10:24
 id: efc08b4d-6250-4967-b2e9-6ebd74df1e8e
 ---
 
-These are recipes for common SQL tasks, quite different from recipes for food like [chicken](example/pkm/personal/chicken_recipe.md).
+These are recipes for common SQL tasks, quite different from recipes for food like [chicken](/pkm/personal/chicken_recipe.md).
--- mndmngr/pkm/services/move_file.py
+++ mndmngr/pkm/services/move_file.py
@@ -4,27 +4,42 @@ import os, sys, dotenv
 dotenv.load_dotenv()
 
 args = sys.argv
-file_path_rel = args[1]
-new_path_rel = args[2]
+file_path_input = args[1]
+new_path_input = args[2]
 
 print("Moving file...")
 
-file_name = file_path_rel.split("/")[-1]
-file_path_full = os.environ["USER_ROOT"] + file_path_rel
+file_path_abs = os.path.abspath(file_path_input)
+new_path_abs = os.path.abspath(new_path_input)
 
-if new_path_rel[-1] == "/":
-    new_path_rel = new_path_rel[:-1]
-new_path_full = os.environ["USER_ROOT"] + new_path_rel + "/" + file_name
+user_root = os.environ["USER_ROOT"]
+
+if not file_path_abs.startswith(user_root):
+    print("Source file not found in project dir. Exiting...")
+    sys.exit(1)
+
+if not new_path_abs.startswith(user_root):
+    print("Dest file not found in project dir. Exiting...")
+    sys.exit(1)
+
+file_path_rel = file_path_abs[len(user_root) :]
+file_name = file_path_rel.split("/")[-1]
+new_path_rel = new_path_abs[len(user_root) :] + "/" + file_name
+full_new_path_abs = new_path_abs + "/" + file_name
+
+print("new path rel" + new_path_rel)
+print("full new path abs" + full_new_path_abs)
+
 
 try:
-    os.rename(file_path_full, new_path_full)
+    os.rename(file_path_abs, full_new_path_abs)
 except:
     print("Could not move file, exiting...")
     sys.exit(1)
 
 print("Updating references...")
 
-for root, dirs, files in os.walk((os.environ["USER_ROOT"] + "/example")):
+for root, dirs, files in os.walk((user_root)):
     for file in files:
         with open(os.path.join(root, file), "r+") as f:
             lines = f.readlines()
