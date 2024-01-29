c6f93a6bcfe352f57e792006c3e0cac347f5bded
Andy Reding <areding99@gmail.com>
2024-01-29 10:22:39 +0000

    add macros, template defs

--- mndmngr/pkm/templates/macros/macro_defs/YearMacro.py
+++ mndmngr/pkm/templates/macros/macro_defs/YearMacro.py
@@ -3,7 +3,7 @@ from mndmngr.pkm.templates.macros.Macro import Macro
 
 class YearMacro(Macro):
     def resolve(self) -> str:
-        return str(self.date.year)
+        return str(self.datetime.year)
 
     def get_key(self) -> str:
         return "$YEAR"
